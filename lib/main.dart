d579886b4f8748639f0f9791dea4a23e2d27f46d
Vinzent <vinzentwitte@gmail.com>
2020-04-20 01:12:06 +0200

    added print hello

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ import 'logic/theme.dart';
 import 'logic/themedata.dart';
 
 void main() {
+  print("hallo");
   WidgetsFlutterBinding.ensureInitialized();
   Getter().getBool(Names.dark).then((isDark) {
     runApp(ChangeNotifierProvider<ThemeChanger>(
