586c5e4e67ddd6f609722f8f20a72c1ff77d8fc7
Vinzent <vinzentwitte@gmail.com>
2020-07-16 15:54:59 +0200

    new IntroScreen

--- lib/main/introScreen.dart
+++ lib/main/introScreen.dart
@@ -4,10 +4,9 @@ import 'package:Vertretung/provider/themedata.dart';
 import 'package:Vertretung/services/authService.dart';
 import 'package:Vertretung/services/cloudDatabase.dart';
 import 'package:flutter/material.dart';
-import 'package:intro_slider/intro_slider.dart';
-import 'package:intro_slider/slide_object.dart';
 import '../logic/localDatabase.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class IntroScreen extends StatefulWidget {
   @override
@@ -20,106 +19,111 @@ class _IntroScreenState extends State<IntroScreen> {
   Widget build(BuildContext context) {
     return Theme(
       data: lightTheme,
-      child: IntroSlider(
-        slides: [
-          Slide(
-            widgetDescription: Center(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "Der bessere Vertretungsplan!",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
-                  ),
-                  SizedBox(
-                    height: 200,
-                  ),
-                  Text(
-                    "Du hast schon ein Account? Dann klick unten auf Anmelden",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  )
-                ],
+      child: IntroductionScreen(
+        pages: [
+          PageViewModel(
+            image: Center(
+              child: Image.asset("assets/icons/icon.png"),
+            ),
+            title: "Vertretung",
+            body: "Der bessere Vertretungsplan!",
+            footer: FlatButton(
+              child: Text("Du hast schon ein Acccount?"),
+              onPressed: () => Navigator.pushNamed(context, Names.logInPage,
+                  arguments: false),
+            ),
+            decoration: PageDecoration(
+              imagePadding: EdgeInsets.symmetric(vertical: 30),
+              titleTextStyle: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(fontSize: 25),
+            ),
+          ),
+          PageViewModel(
+            title: "Features",
+            bodyWidget: Text(
+              "-Personalisierte Vertretung\n"
+              "\n-Personalisierte Benachrichtigungen\n"
+              "\n-Kein lästiges Reinzoomen in der Dsb-Mobile App\n"
+              "\n-Du siehst die Vertretung deiner Freunde\n"
+              "\n-Verteilung von Nachrichten direkt über die App\n"
+              "\n-Dark/Light Mode\n"
+              "\n-Anzeige der aktuellen Wochennummer",
+              style: TextStyle(fontSize: 20),
+            ),
+            decoration: PageDecoration(
+              titlePadding: EdgeInsets.symmetric(vertical: 50),
+              titleTextStyle: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            backgroundColor: Colors.blue[800],
           ),
-          Slide(
-            title: "Features:",
-            centerWidget: Container(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "-Personalisierte Vertretung\n"
-                "\n-Du bekommst SINNVOLLE Benachrichtigungen\n"
-                "\n-Kein lästiges Reinzoomen in der Dsb-Mobile App\n"
-                "\n-Du siehst immer was deine Freunde für Vertretung haben\n"
-                "\n-Verteilung von Nachrichten direkt über die App\n"
-                "\n-Durchgehender Dark/Light Mode\n"
-                "\n-Anzeige der aktuellen Wochennummer",
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
+          PageViewModel(
+            title: "In welcher Klasse/Stufe bist du?",
+            bodyWidget: StufenList(),
+            decoration: PageDecoration(
+              titlePadding: EdgeInsets.symmetric(vertical: 100),
+              titleTextStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            backgroundColor: Colors.blue[800],
           ),
-          Slide(
-            title: "In welcher Stufe/Klasse bist du?",
-            maxLineTitle: 3,
-            styleTitle: TextStyle(fontSize: 25, color: Colors.white),
-            centerWidget: Theme(
-              data: lightTheme,
-              child: StufenList(),
-            ),
-            backgroundColor: Colors.blue[800],
-          ),
-          Slide(
-            title: "Wie ist dein Name?",
-            maxLineTitle: 3,
-            styleTitle: TextStyle(fontSize: 25, color: Colors.white),
-            centerWidget: Container(
-              padding: EdgeInsets.all(100),
+          PageViewModel(
+            title: "Wie heißt du?",
+            bodyWidget: Container(
+              padding: EdgeInsets.symmetric(horizontal: 50),
               child: TextField(
                 controller: nameController,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 20,
-                  color: Colors.white,
                 ),
               ),
             ),
-            backgroundColor: Colors.blue[800],
+            footer: Text("Für das Freunde Feature nötig."),
+            decoration: PageDecoration(
+              titlePadding: EdgeInsets.symmetric(vertical: 100),
+              titleTextStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          Slide(
-              title: "Fertig",
-              backgroundColor: Colors.blue[800],
-              widgetDescription: Column(
-                children: <Widget>[
-                  Text(
-                    "Den Rest der Einstellungen, wie deine Fächer, sowie Hilfe, zu verschiedenen Themen findest du unter Vertretung oben rechts.\n\n\n",
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  InkWell(
-                    child: Text(
-                      "Außerdem bist du damit einverstanden, dass deine Einstellungen in der Cloud(https://firebase.google.com/) gespeichert werde. Dies ist für die  Bereitstellung der Benachrichtigungen und des Freundes Feature nötig.",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: () {
-                      launch("https://firebase.google.com/");
-                    },
-                  ),
-                ],
-              )),
+          PageViewModel(
+            title: "Fertig",
+            bodyWidget: Column(
+              children: <Widget>[
+                Text(
+                  "Den Rest der Einstellungen, wie deine Fächer, sowie Hilfe, zu verschiedenen Themen findest du unter Vertretung oben rechts. \n\n",
+                  style: TextStyle(fontSize: 19),
+                ),
+                Text(
+                  "Außerdem bist du damit einverstanden, dass deine Einstellungen in der Cloud (Firebase) gespeichert werden. Dies ist für die  Bereitstellung der Benachrichtigungen und des Freundes Feature nötig.",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+            footer: FlatButton(
+              child: Text("Zu Firebase"),
+              onPressed: () => launch("https://firebase.google.com/"),
+            ),
+            decoration: PageDecoration(
+              titlePadding: EdgeInsets.symmetric(vertical: 50),
+              titleTextStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ],
-        colorDot: Colors.white,
-        colorActiveDot: Colors.blue[900],
-        nameSkipBtn: "Anmelden",
-        widthSkipBtn: 100,
-        onSkipPress: () =>
-            Navigator.pushNamed(context, Names.logInPage, arguments: false),
-        onDonePress: () async {
+        next: Icon(Icons.arrow_forward),
+        showNextButton: true,
+        done: Text("Fertig"),
+        onDone: () async {
           await AuthService().signInAnon();
           String name = nameController.text;
           if (name == "") name = "Nicht festgelegt";
