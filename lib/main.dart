520ccd2797154762a22cf5e4080860b80be7d34c
Vinzent <vinzentwitte@gmail.com>
2020-10-03 20:18:37 +0200

    remove provider for authState

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ import 'package:Vertretung/pages/helpPage.dart';
 import 'package:Vertretung/main/introScreen.dart';
 import 'package:Vertretung/authentication/accountPage.dart';
 import 'package:Vertretung/main/wrapper.dart';
-import 'package:Vertretung/services/authService.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:Vertretung/logic/wiredashKeys.dart';
@@ -22,7 +20,6 @@ import 'package:Vertretung/provider/providerData.dart';
 import 'package:Vertretung/provider/themedata.dart';
 import 'package:Vertretung/settings/settingsPage.dart';
 import 'main/splash.dart';
-import 'dart:async';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
@@ -44,40 +41,36 @@ class MyAppSt extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<ProviderData>(context);
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      //used for the feedback function
-      child: Wiredash(
-        options: WiredashOptionsData(showDebugFloatingEntryPoint: false),
-        theme: WiredashThemeData(brightness: provider.getUsedTheme()),
+    return Wiredash(
+      options: WiredashOptionsData(showDebugFloatingEntryPoint: false),
+      theme: WiredashThemeData(brightness: provider.getUsedTheme()),
+      navigatorKey: MyKeys.navigatorKey,
+
+      //replace with your own keys from wiredash.io or remove the Wiredash Widget
+      secret: WiredashKeys.secret,
+      projectId: WiredashKeys.id,
+
+      child: MaterialApp(
         navigatorKey: MyKeys.navigatorKey,
-
-        //replace with your own keys from wiredash.io or remove the Wiredash Widget
-        secret: WiredashKeys.secret,
-        projectId: WiredashKeys.id,
-
-        child: MaterialApp(
-          navigatorKey: MyKeys.navigatorKey,
-          navigatorObservers: [
-            FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
-          ],
-          theme: lightTheme,
-          darkTheme: darkTheme,
-          themeMode: provider.getThemeMode(),
-          initialRoute: Names.splashScreen,
-          routes: {
-            Names.splashScreen: (context) => Splash(),
-            Names.wrapper: (context) => Wrapper(),
-            Names.settingsPage: (context) => SettingsPage(),
-            Names.helpPage: (context) => HelpPage(),
-            Names.introScreen: (context) => IntroScreen(),
-            Names.newsPage: (context) => NewsPage(),
-            Names.aboutPage: (context) => AboutPage(),
-            Names.accountPage: (context) => AccountPage(),
-            Names.friendsList: (context) => FriendsList(),
-            Names.changePasswordPage: (context) => ChangePasswordPage(),
-          },
-        ),
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
+        ],
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: provider.getThemeMode(),
+        initialRoute: Names.splashScreen,
+        routes: {
+          Names.splashScreen: (context) => Splash(),
+          Names.wrapper: (context) => Wrapper(),
+          Names.settingsPage: (context) => SettingsPage(),
+          Names.helpPage: (context) => HelpPage(),
+          Names.introScreen: (context) => IntroScreen(),
+          Names.newsPage: (context) => NewsPage(),
+          Names.aboutPage: (context) => AboutPage(),
+          Names.accountPage: (context) => AccountPage(),
+          Names.friendsList: (context) => FriendsList(),
+          Names.changePasswordPage: (context) => ChangePasswordPage(),
+        },
       ),
     );
   }
--- lib/main/wrapper.dart
+++ lib/main/wrapper.dart
@@ -1,23 +1,24 @@
 import 'package:Vertretung/authentication/webPageLogIn.dart';
 import 'package:Vertretung/main/introScreen.dart';
+import 'package:Vertretung/services/authService.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<User>(context);
-    if (user == null) {
-      if (kIsWeb)
-        return WebPageLogIn(isLogIn: true,);
-      else
-        return IntroScreen();
-    } else {
-      return Home();
-    }
+    return StreamBuilder(
+      stream: AuthService().user,
+      builder: (context, snapshot) {
+        if (snapshot.hasData)
+          return Home();
+        else if (kIsWeb)
+          return WebPageLogIn(isLogIn: true);
+        else
+          return IntroScreen();
+      },
+    );
   }
 }
